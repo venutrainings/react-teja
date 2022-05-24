b798a4b4ef3a76fa83a9a5e7e83c53e286c34738
DharmaTejaAllada1 <anssd.teja@gmail.com>
2022-05-24 16:17:42 +0530

    update method complete

--- hooks-crud/src/component/pages/UpdateRecord.js
+++ hooks-crud/src/component/pages/UpdateRecord.js
@@ -1,103 +1,104 @@
-import axios from 'axios';
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import {useNavigate, useParams} from 'react-router-dom';
 
-
-const UpdateRecord = (props) =>{
-
+ 
+function UpdateRecord() {
+  const navigate = useNavigate();
   let {id} = useParams();
 
-  console.log("currernt" + id);
+  const [users, setUsers] = useState([]);
 
-    const navigate = useNavigate();
-    const [users, setUsers] = useState([]);
 
-    const [newData, setNewData] = useState({
+  useEffect(() => {
+    getData()
+  }, []);
 
-        "name": "",
-        "email": "",
-        "gender": "",
-        "status": "",
-      });
+  const getData = () => {
 
-      const [current,  setCurrent] =useState([]);
+    axios({
+      method: 'GET', //you can set what request you want to be
+      url: `https://gorest.co.in/public/v2/users/${id}`,
+      headers: {
+        Authorization: 'Bearer 489d544765eae2a983ad8969b8ff4f5960e934844f4171ae740b06683a916561'
+      }
+    })
+      .then(res =>{
+        alert("VIEW");
+        setUsers(res.data)
+      })     
 
-      useEffect(() => {
-        
-        getData() }, []);
-     
-       const getData =() => {
-         axios.get(`https://gorest.co.in/public/v2/users/${id}`)
-           .then(res => {
-             setUsers(res.data)
-             console.log(res.data)
-           })
-           .catch(err => {
-             console.log(err)
-           })
-       
-       }
-
-      // const onUpdateData = ({ name, value }) => {
-        
-      //   setNewData({ ...newData, [name]: value} );
-      // };
-
-      const UpdateRecord = async( id, e)  => {
-        e.preventDefault();
-       
-         await  axios({
-        method: 'put', //you can set what request you want to be
-        url: `https://gorest.co.in/public/v2/users/${id}`,
-        data: users,
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          Authorization: 'Bearer 489d544765eae2a983ad8969b8ff4f5960e934844f4171ae740b06683a916561'
-        }
-      }).then(res =>{
-        navigate("/")  
-        alert("updated")  
-      }).catch(err =>{
-          alert(err)
+      .catch(err => {
+        alert(err)
       })
-      };
+
+    }
+  
+  const [newData, setNewData] = useState({
+    "name": "",
+    "email": "",
+    "gender": "",
+    "status": "",
+  });
+
+  const onUpdateData = ({ name, value }) => {
+    setUsers({ ...users, [name]: value} );
+  };
+
+  const UpdateRecord = async e => {
+    e.preventDefault();
+  await  axios({
+    method: 'put', //you can set what request you want to be
+    url: `https://gorest.co.in/public/v2/users/ ${id}`,
+    data: users,
+    headers: {
+      Authorization: 'Bearer 489d544765eae2a983ad8969b8ff4f5960e934844f4171ae740b06683a916561'
+    }
+  }).then(res =>{
+    navigate("/")  
+    alert("updated")  
+  }).catch(err =>{
+      alert(err)
+  })
+  };
+
+  return (
+    <div className="container">
+    <h1>Update Record</h1>
+
+    <form onSubmit={e => {
+      e.preventDefault();
+    }}>
+    <div className="row">
+      < div className="col-6 mb-3">
+        <label className="form-label">Name</label>
+        <input value={users.name || ''} onChange={(e) => onUpdateData(e.target)} className="form-control" name="name" ></input>
+      </div>
+      <div className="col-6 mb-3">
+        <label className="form-label">E-mail</label>
+        <input value={users.email || ''} onChange={(e) => onUpdateData(e.target)} className="form-control" name="email" type='email'></input>
+      </div>
+    </div>
+
+    <div className="row">
+
+      <div className="col-6 mb-3">
+        <label className="form-label">Gender</label>
+        <input value={users.gender || ''} onChange={(e) => onUpdateData(e.target)} className="form-control" name="gender"></input>
+      </div>
+
+      <div className="col-6 mb-3">
+        <label className="form-label">Status</label>
+        <input value={users.status || ''} onChange={(e) => onUpdateData(e.target)} className='form-control' name="status"></input>
+      </div>
+
+    </div>
+
+    <button onClick={(e) => UpdateRecord(e)}>Submit</button>
+  </form>
+</div>
  
-    return(
-        <div className="container">
-            <h1>Update Record</h1>
- 
-            <form>
-            <div className="row"> 
-              < div className="col-6 mb-3">
-                <label className="form-label">Name</label>
-
-                <input type='text' value={users.name || ''} onChange={(e) => setUsers(e.target.value)} className="form-control" name="name"   ></input>
-              </div>
-              <div className="col-6 mb-3">
-                <label className="form-label">E-mail</label>
-                <input value={users.email || ''} onChange={(e) => setUsers(e.target.value)} className="form-control" name="email" type='email'></input>
-              </div>
-            </div>
-
-            <div className="row">
-
-              <div className="col-6 mb-3">
-                <label className="form-label">Gender</label>
-                <input value={users.gender || ''} onChange={(e) => setUsers(e.target.value)} className="form-control" name="gender"></input>
-              </div>
-
-              <div className="col-6 mb-3">
-                <label className="form-label">Status</label>
-                <input value={users.status || ''} onChange={(e) => setUsers(e.target.value)} className='form-control' name="status"></input>
-              </div>
-
-            </div>
-
-            <button onClick={(e) => UpdateRecord(e)}>Submit</button>
-          </form>
-        </div>
-    )
+  )
 }
 
 export default UpdateRecord;
